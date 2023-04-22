a73c51f186a376fa579584ec049a2cb671ce29bc
Fco Javier Rivas Martin <rfrancisco31@hotmail.com>
2023-04-22 17:38:59 +0200

    First prototype

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : Enemy
+{
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public Transform homePosition;
+    public float chaseRadius;
+    public float attackRadius;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            if(currentState == EnemyState.idle || currentState == EnemyState.walk && Vector3.Distance(target.position, transform.position) > attackRadius)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);                
+                ChangeState(EnemyState.walk);                
+                anim.SetBool("wakeUp",true);
+            }            
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
+        }
+        else
+        {
+            anim.SetBool("wakeUp", false);
+        }
+        
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if(currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact,
+    stagger,
+    idle
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+    public PlayerState currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack && currentState != PlayerState.stagger)
+        {
+            StartCoroutine(AttackCo());
+        }
+
+    }
+    void FixedUpdate()
+    {
+        change = Vector3.zero;
+        change.y = Input.GetAxisRaw("Vertical");
+        change.x = Input.GetAxisRaw("Horizontal");
+        if (currentState == PlayerState.walk || currentState == PlayerState.idle)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    // Update is called once per frame. Used FixedUpdate because Update() makes the character walk slower.
+    //void FixedUpdate()
+    //{
+    //    change = Vector3.zero;
+    //    change.x = Input.GetAxisRaw("Horizontal");
+    //    change.y = Input.GetAxisRaw("Vertical");
+
+    //    if (Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+    //    {
+    //        StartCoroutine(AttackCo());
+    //    } else if(currentState == PlayerState.walk)
+    //    {
+    //        UpdateAnimationAndMove();
+    //    }        
+    //}
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidBody.MovePosition(
+            transform.position + change.normalized * speed * Time.deltaTime
+            );
+    }
+
+    public void Knock(float knockTime)
+    {
+        StartCoroutine(KnockCo(knockTime));
+    }
+
+    private IEnumerator KnockCo(float knockTime)
+    {
+        if (myRigidBody != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            myRigidBody.velocity = Vector2.zero;
+            currentState = PlayerState.idle;
+            myRigidBody.velocity = Vector2.zero;
+        }
+    }
+}
